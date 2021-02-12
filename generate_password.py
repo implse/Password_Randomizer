8724609d2def6eba53c38f08ce2df3ca10f8ee9f
Finite <finite.impulse@gmail.com>
2021-02-12 23:11:53 +0100

    Add input for password length

--- generate_password.py
+++ generate_password.py
@@ -3,6 +3,21 @@ import secrets
 
 alphanumeric = string.ascii_letters + string.digits
 symbols = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-password = "".join(secrets.choice(alphanumeric + symbols) for char in range(20))
 
+
+while True:
+    try:
+        ask_user = input("How many characters do you want in your password: ")
+        password_length = int(ask_user)
+        if password_length > 0:
+            break
+        else:
+            print("Number can't be negative or zero, try again\n")
+            continue
+    except:
+        print("Input must be an integer, try again\n")
+        continue
+
+
+password = "".join(secrets.choice(alphanumeric + symbols) for char in range(password_length))
 print(password)
